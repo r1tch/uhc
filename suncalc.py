8ed0d2bffb3faea540cfc998a750afb806099d8b
Richard G. Roman <rits@rits.hu>
2016-05-14 00:24:27 +0200

    Fixed sunrise-set times, tz independently

--- suncalc.py
+++ suncalc.py
@@ -1,14 +1,12 @@
 #!env python
 """
-Library to calculate the sunset
-Written by Michel Anders
-http://michelanders.blogspot.fr/2010/12/<br>
-        calulating-sunrise-and-sunset-in-python.html
+Original by Michel Anders
+http://michelanders.blogspot.fr/2010/12/calulating-sunrise-and-sunset-in-python.html
+
+...adjusted for returning timestamps as seconds, using UTC always
 """
 
-import tzlocal
-
-from datetime import datetime, time
+from datetime import datetime, time, timezone
 from math import cos, sin, acos, asin, tan
 from math import degrees as deg, radians as rad
 
@@ -17,12 +15,6 @@ class SunCalc:
     """
     Calculate sunrise and sunset based on equations from NOAA
     http://www.srrb.noaa.gov/highlights/sunrise/calcdetails.html
-    typical use, calculating the sunrise at the present day:
-
-    import datetime
-    import sunrise
-    s = sun(lat=49,long=3)
-    print('sunrise at ',s.sunrise(when=datetime.datetime.now())
     """
 
     def __init__(self, lat=52.8948846, lon=10.4468234):
@@ -32,9 +24,8 @@ class SunCalc:
         self.coord = (lat, lon)
         self.day = None
         self.time = None
-        self.timezone = None
 
-    def sunrise(self, when=datetime.now(tz=tzlocal.get_localzone())):
+    def sunrise(self, when=datetime.now()):
         """
         return the time of sunrise as a datetime.time object
         when is a datetime.datetime object. If none is given
@@ -45,7 +36,7 @@ class SunCalc:
         self.__calc()
         return SunCalc.__timefromdecimalday(self.sunrise_t)
 
-    def sunset(self, when=datetime.now(tz=tzlocal.get_localzone())):
+    def sunset(self, when=datetime.now()):
         """
         returns datetime when sun will set
         """
@@ -53,7 +44,7 @@ class SunCalc:
         self.__calc()
         return SunCalc.__timefromdecimalday(self.sunset_t)
 
-    def solarnoon(self, when=datetime.now(tz=tzlocal.get_localzone())):
+    def solarnoon(self, when=datetime.now()):
         """
         datetime of solar noon
         """
@@ -64,17 +55,9 @@ class SunCalc:
     @staticmethod
     def __timefromdecimalday(day):
         """
-        returns a datetime.time object.
-        day is a decimal day between 0.0 and 1.0, e.g. noon = 0.5
+        returns a seconds from epoch
         """
-        hours = 24.0 * day
-        hour = int(hours)
-        minutes = (hours - hour) * 60
-        minute = int(minutes)
-        seconds = (minutes - minute) * 60
-        second = int(seconds)
-        res = time(hour=hour, minute=minute, second=second)
-        return res
+        return int(day*60*60*24)
 
     def __preptime(self, when):
         """
@@ -86,15 +69,7 @@ class SunCalc:
         # OpenOffice spreadsheets with days numbered from
         # 1/1/1900. The difference are those numbers taken for
         # 18/12/2010
-        self.day = when.toordinal() - (734124 - 40529)
-        wtime = when.time()
-        self.time = (wtime.hour + wtime.minute / 60.0 + wtime.second / 3600.0) / 24.0
-
-        offset = when.utcoffset()
-        if offset is None:
-            raise ValueError("tzone should be set")
-
-        self.timezone = offset.seconds / 3600.0
+        self.day = when.toordinal() - (734124 - 40528)
 
     def __calc(self):
         """
@@ -104,18 +79,15 @@ class SunCalc:
         sunrise_t, sunset_t and solarnoon_t
         """
         # in hours, east is positive
-        timezone = self.timezone
         latitude = self.coord[0]
         longitude = self.coord[1]
 
-        # percentage past midnight, i.e. noon  is 0.5
-        some_time = self.time
         # daynumber 1=1/1/1900
         some_day = self.day
 
         # Julian day
-        jday = some_day + 2415018.5 + some_time - (timezone / 24)
-        # Julian jentury
+        jday = some_day + 2415020.5
+        # Julian century
         jcent = (jday - 2451545) / 36525
 
         Manom = 357.52911 + jcent * (35999.05029 - 0.0001537 * jcent)
@@ -137,20 +109,15 @@ class SunCalc:
         hourangle = deg(acos(cos(rad(90.833)) / (cos(rad(latitude)) * cos(rad(declination))) - tan(rad(latitude)) * tan(
             rad(declination))))
 
-        self.solarnoon_t = (720 - 4 * longitude - eqtime + timezone * 60) / 1440
+        self.solarnoon_t = (720 - 4 * longitude - eqtime) / 1440 + some_day - 25567
         self.sunrise_t = self.solarnoon_t - hourangle * 4 / 1440
         self.sunset_t = self.solarnoon_t + hourangle * 4 / 1440
 
 
 if __name__ == "__main__":
-    # s=sun(lat=52.37,long=4.90) #amsterdam
-    sun = SunCalc(lat=47.48094, lon=19.01664)
-    mytz = tzlocal.get_localzone()
-    print("mytz:{}".format(repr(mytz)))
-    when = datetime.now(tz=mytz)
-    print("x:{}".format(when))
-    when = datetime(2016,5,13,0,0,0, tzinfo=mytz)
-    print("x:{}".format(when))
-    when = when.astimezone(mytz)
-    print("x:{} - {}".format(repr(when), repr(mytz)))
-    print(sun.sunrise(when), sun.solarnoon(when), sun.sunset(when))
+    sun = SunCalc(lat=47.48094, lon=19.01664)   # Budapest, Hungary
+    when = datetime.now()
+    print("x:{}".format(repr(when)))
+    when = datetime(2016,3,27,1,2,3, tzinfo=timezone.utc)
+    print("x:{}".format(repr(when)))
+    print(datetime.fromtimestamp(sun.sunrise(when)), datetime.fromtimestamp(sun.solarnoon(when)), datetime.fromtimestamp(sun.sunset(when)))
