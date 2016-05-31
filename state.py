2ecb97db7b524e8a218aabbc4c126bbb3b39af69
Richard G. Roman <rits@rits.hu>
2016-05-31 07:40:40 +0200

    atHome false by default for remote restarts

--- state.py
+++ state.py
@@ -5,7 +5,7 @@
 class State:
     """Common object accessible by all services, to store current states"""
     def __init__(self):
-        self.atHome = True   # we're home if alarm system is unarmed
+        self.atHome = False   # we're home if alarm system is unarmed
         self.asleep = False  # detected based on movements around the house
         self.flashAlarmState = "off"    # "delay", "preflash", "full"
         self.guestHost = False # if true, no automation when home
